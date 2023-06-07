a6e52a79f50da4be857dde6a100a63579149ed11
ciwanridwan <ridwan.andalus79@gmail.com>
2023-06-08 00:59:56 +0700

    first commit and auth layout

---
+++ src/ecommerce_dashboard/Ecommerce_dashboard.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ecommerce_dashboard;
+
+import authentications.login;
+
+/**
+ *
+ * @author ciwan
+ */
+public class Ecommerce_dashboard {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new login().setVisible(true);
+    }
+    
+}
